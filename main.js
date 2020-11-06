7a7306b7919f2eb128b53b3c5e42a92d6cccd34c
Jean-Francois BATAILLE <63928409+jfrancois974@users.noreply.github.com>
2020-11-06 11:03:13 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+// NAVBAR
+let lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function () {
+    const scrollTop = window.pageTOffset || this.document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-50px";
+    } else {
+        navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+
+});
+
+// TYPED
+let typed = new Typed('.typed', {
+    strings: ["Bonjour , je me présente je m'appelle Jean-François", "Après une carrière m'ayant fait découvrir plusieurs milieux professionnels et exercer plus de 15 métiers différents. J'ai décidé de me lancer à fond dans l'apprentissage du code, depuis près de 4 mois je me forme à temps plein à l'AFPA Marseille Saint-Jérome aux langages Front et Back tels que CSS, Javascript et son Framework React, PHP et MySQl afin devenir le plus rapidement possible développeur. Plus qu'un avenir professionnel, je me suis découvert une passion, celle de coder."],
+    typeSpeed: 20,
+});
+
+// COMPTEUR LIVE
+let compteur = 0;
+
+$(window).scroll(function () {
+
+    const top = $('.counter').offset().top - window.innerHeight;
+
+    if (compteur === 0 && $(window).scrollTop() > top) {
+        $('.counter-value').each(function () {
+            let $this = $(this),
+            countTo = $this.attr('data-count');
+
+            $({
+                countNum : $this.text()
+            }).animate({
+                countNum : countTo
+            }, 
+            {
+                duration: 10000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                    $this.text(this.countNum);
+                }
+            });
+        });
+        compteur = 1;
+    }
+});
+
+//AOS
+AOS.init();
